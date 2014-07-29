73146e64efa641fefe261d1d3e0c1c1e43731ed6
Paul <Paul@Pauls-MacBook-Pro.local>
2014-07-29 14:49:05 +0200

    removed ball from ballview and renamed it to main view (forgot main activity)

--- workspace/BouncingBall/src/com/example/bouncingball/MainActivity.java
+++ workspace/BouncingBall/src/com/example/bouncingball/MainActivity.java
@@ -1,18 +1,14 @@
 package com.example.bouncingball;
   
-import java.util.Random;
-
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnTouchListener;
 import android.view.Window;
 import android.view.WindowManager;
   
-public class MainActivity extends Activity implements OnTouchListener {
+public class MainActivity extends Activity {
 
 	private RotationState rotationState;
    /** Called when the activity is first created. */
@@ -20,7 +16,7 @@ public class MainActivity extends Activity implements OnTouchListener {
    public void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
        rotationState = new RotationState(this);
-      View bouncingBallView = new BouncingBallView(this, rotationState);
+      View bouncingBallView = new MainView(this, rotationState);
 
       requestWindowFeature(Window.FEATURE_NO_TITLE);
       getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
@@ -28,7 +24,6 @@ public class MainActivity extends Activity implements OnTouchListener {
       setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
       setContentView(bouncingBallView);
       bouncingBallView.setBackgroundColor(Color.BLACK);
-      bouncingBallView.setOnTouchListener(this);
    }
    
    
@@ -44,32 +39,6 @@ public class MainActivity extends Activity implements OnTouchListener {
 		   this.rotationState.stop();
    }*/
    
-   
-   @Override
-   public boolean onTouch(View v, MotionEvent event)
-   {
-	   if(v instanceof BouncingBallView)
-	   {
-		BouncingBallView bv = (BouncingBallView) v;
-		Random rand = new Random();
-		int r, g, b;
-		r = rand.nextInt(255);
-		g = rand.nextInt(255);
-		b = rand.nextInt(255);
-		float ex =	event.getX();
-		float ey = 	event.getY();
-		Ball ball = bv.ballList.get(0);
-		if(ball != null)
-		{
-			if(ball.isNear(ex, ey, 50.0f))
-			{
-			ball.color = Color.rgb(r, g,b );
-			//bv.ballList.clear();
-			}
-		}
-	   }
-	   return true;
-   }
 
    
   
