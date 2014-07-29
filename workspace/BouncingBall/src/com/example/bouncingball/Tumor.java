c7d223de224d192cf97d15a954a3e96c663afd82
Paul <Paul@Pauls-MacBook-Pro.local>
2014-07-29 12:21:06 +0200

    renamed tumor in tumorview

--- workspace/BouncingBall/src/com/example/bouncingball/Tumor.java
+++
@@ -1,9 +0,0 @@
-package com.example.bouncingball;
-
-import android.media.Image;
-
-public class Tumor {
-
-	Image tumor_img = null;
-	
-}
---
+++ workspace/BouncingBall/src/com/example/bouncingball/TumorView.java
@@ -0,0 +1,20 @@
+package com.example.bouncingball;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.View;
+import android.widget.ImageView;
+
+public class TumorView extends ImageView {
+
+	ImageView tumor_img = null;
+	
+	
+	public TumorView(Context context){
+		super(context);
+		
+		//tumor_img = new ImageView();
+		
+	}
+	
+}
